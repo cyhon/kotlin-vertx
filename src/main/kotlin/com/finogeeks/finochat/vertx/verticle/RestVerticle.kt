63a803800f3ecaf2d8d98b3b4c518b20063245f3
linhaitao <linhaitao@finogeeks.com>
2017-11-27 19:22:51 +0800

    error processing

--- src/main/kotlin/com/finogeeks/finochat/vertx/verticle/RestVerticle.kt
+++ src/main/kotlin/com/finogeeks/finochat/vertx/verticle/RestVerticle.kt
@@ -5,11 +5,15 @@ import com.finogeeks.finochat.vertx.handler.NaiveHandlerFactory
 import com.finogeeks.finochat.vertx.router.BasicRouter
 import com.google.inject.Guice
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.logging.LoggerFactory
+import kotlin.system.exitProcess
 
 /**
  * Created by hetiu 2017/8/20.<br/>
  */
 class RestVerticle : AbstractVerticle() {
+
+    private val LOG = LoggerFactory.getLogger("RestVerticle")
 
     override fun start() {
 
@@ -18,10 +22,15 @@ class RestVerticle : AbstractVerticle() {
             injector.injectMembers(this)
             val handlerFactory = injector.getInstance(NaiveHandlerFactory::class.java)
             it.complete(handlerFactory)
-        }, {
-            val router = BasicRouter(vertx, it.result())
-            val port = config().getInteger("rest.server.http.port", 8080)
-            vertx.createHttpServer().requestHandler({ router.accept(it) }).listen(port)
-        })
+        }) {
+            if (it.succeeded()) {
+                val router = BasicRouter(vertx, it.result())
+                val port = config().getInteger("rest.server.http.port", 8080)
+                vertx.createHttpServer().requestHandler({ router.accept(it) }).listen(port)
+            } else {
+                LOG.error(it.cause())
+                exitProcess(-1)
+            }
+        }
     }
 }
